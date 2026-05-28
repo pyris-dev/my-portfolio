778d765921699da5b8440b3dc21add001e151103
Nathan Schilling <99233134+pyris-dev@users.noreply.github.com>
2026-05-28 14:40:43 -0700

    Remove Dist and add Githhub Pages Build Workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1];
+const base = process.env.GITHUB_ACTIONS === "true" && repoName ? `/${repoName}/` : "/";
+
 // https://vite.dev/config/
 export default defineConfig({
+  base,
   plugins: [react()],
   preview: {
     allowedHosts: ["pyris.dev", "www.pyris.dev"],
